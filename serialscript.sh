604f1f4eda14762e4f9eb32a13728eae00ef0b7b
erendabn <eren.daban@hotmail.com>
2025-07-29 17:49:11 +0300

    script added

--- serialscript.sh
+++ serialscript.sh
@@ -8,6 +8,9 @@ sudo cp /etc/mdev.conf /etc/mdev.conf.bak
 
 sudo sed -i '/^ttyUSB\[0-9\]\*\s\+root:root 660\s*$/d' /etc/mdev.conf
 
-sudo echo -e 'ttyUSB[0-9]*\troot:root 660 * /etc/init.d/S50seriallink' >> /etc/mdev.conf
+echo -e 'ttyUSB[0-9]*\troot:root 660 * /etc/init.d/S50seriallink' >> /etc/mdev.conf
 
 sudo mdev -s
+
+sudo /etc/init.d/S50seriallink start
+
